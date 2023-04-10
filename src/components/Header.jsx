d5d28e255ba1463ccc0fed2b2ea7bc24bc9cbc58
Shajib-Dv <mdsojeeb242@gmail.com>
2023-04-10 10:08:40 +0600

    fake data done

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h3 className="text-4xl text-yellow-800">Header</h3>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
